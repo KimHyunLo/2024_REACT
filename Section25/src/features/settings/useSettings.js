f3dd61e9f1d47355079a29d34825545655e6631a
현로 <leeas016>
2024-05-20 21:01:04 +0900

    fix: section25

--- Section25/src/features/settings/useSettings.js
+++ Section25/src/features/settings/useSettings.js
@@ -2,11 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getSettings } from "../../services/apiSettings";
 
 export function useSettings() {
-  const {
-    isLoading,
-    error,
-    data: settings,
-  } = useQuery({
+  const { isLoading, data: settings } = useQuery({
     queryKey: ["settings"],
     queryFn: getSettings,
   });
